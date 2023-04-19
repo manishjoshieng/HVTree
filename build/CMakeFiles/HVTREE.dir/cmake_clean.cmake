3890070906a1d767bc8a64006ad7a049b9a10a5d
manishjoshi34 <manish.joshi.eng@gmail.com>
2023-04-19 23:09:35 +0530

    fix bug

---
+++ build/CMakeFiles/HVTREE.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HVTREE.dir/main.cpp.o"
+  "CMakeFiles/HVTREE.dir/main.cpp.o.d"
+  "CMakeFiles/HVTREE.dir/src/HVTree.cpp.o"
+  "CMakeFiles/HVTREE.dir/src/HVTree.cpp.o.d"
+  "HVTREE"
+  "HVTREE.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HVTREE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/mysoft.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mysoft.
+# This may be replaced when dependencies are built.
